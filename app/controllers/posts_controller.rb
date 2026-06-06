722fee4accd59874844c2734418adb842c6ab797
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-06-06 15:34:50 +0300

    Create the seven CRUD operations for the Post resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,56 @@
 class PostsController < ApplicationController
-  # your code goes here
+
+# GET /posts
+def index
+  @posts = Post.all
+end
+
+# GET /posts/new
+def new
+  @post = Post.new()
+end
+
+# POST /posts
+def create
+  @post = Post.new(post_params)
+  if @post.save
+    redirect_to @post, notice: "Post created"
+  else
+    render :new, status: :unprocessable_content
+  end
+end
+
+# GET /posts/:id
+def show
+  @post = Post.find(params[:id])
+end
+
+# GET /posts/:id/edit
+def edit
+  @post = Post.find(params[:id])
+end
+
+# PATCH /posts/:id
+def update
+  @post = Post.find(params[:id])
+  if @post.update(title: params[:post][:title], content: params[:post][:content])
+    redirect_to @post, notice: "Post updated"
+  else
+    render :edit, status: :unprocessable_content
+  end
+end
+
+# DELETE /posts/:id
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to @post, notice: "Post Deleted"
+end
+
+private
+
+def post_params
+  params.expect(post: [:title, :content])
+end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  attribute :title, :string
+  attribute :content, :string
 end
